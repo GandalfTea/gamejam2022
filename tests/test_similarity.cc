4f32acb5490cabfd6650767eae3315ead9eef754
Octavian Rusu <octavian.concept@gmail.com>
2022-07-05 14:22:35 +0100

    Similarity test passes

--- tests/test_similarity.cc
+++ tests/test_similarity.cc
@@ -1,13 +1,25 @@
 
-#include "../src/decrypt.cc";
 #include "../include/core.h";
+#include "../include/similarity.h";
 
 #include <cassert>
+#include <cmath>
+#include <iomanip>
+
+#define EPSILON 0.00001
 
 using namespace std;
 using namespace gamejam;
 
 
+bool compare( float a, float b) {
+	return fabs(a-b) <= EPSILON;
+}
+
 int main() {
+	std::cout << std::fixed << std::setprecision(50);
+
 	assert( get_similarity("Hello", "Hello") == 1);	
+	assert( compare( get_similarity("std::string", "C-string"), 0.545455));
+	std::cout << "\nPass\n";
 }
